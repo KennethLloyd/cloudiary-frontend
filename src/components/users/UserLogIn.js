9c8821970fc84d8039d3343cde6c818b47898f34
KennethLloyd <verastiguelloyd@gmail.com>
2020-06-14 10:27:09 +0800

    Add reactstrap components to login

--- src/components/users/UserLogIn.js
+++ src/components/users/UserLogIn.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { logIn } from '../../actions';
 import { Field, reduxForm } from 'redux-form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form, FormGroup, Input, Container } from 'reactstrap';
+
+import { logIn } from '../../actions';
 
 class UserLogIn extends React.Component {
   renderError({ error, touched }) {
@@ -14,42 +17,53 @@ class UserLogIn extends React.Component {
     }
   }
 
-  renderInput = (formProps) => {
-    const { input, label, meta } = formProps;
-    //const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+  renderInput = formProps => {
+    console.log(formProps);
+    const { input, meta, type, id, placeholder } = formProps;
+
     return (
       <div>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
+        <Input {...input} type={type} id={id} placeholder={placeholder} />
         <div>{this.renderError(meta)}</div>
       </div>
     );
   };
 
-  onSubmit = (formValues) => {
+  onSubmit = formValues => {
+    console.log(formValues);
     this.props.logIn(formValues);
   };
 
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Field
-          name="email"
-          component={this.renderInput}
-          label="Email Address: "
-        />
-        <Field
-          name="password"
-          component={this.renderInput}
-          label="Password: "
-        />
-        <button>Log In</button>
-      </form>
+      <Container>
+        <Form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <FormGroup>
+            <Field
+              type="email"
+              name="email"
+              id="userEmail"
+              placeholder="Email Address"
+              component={this.renderInput}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Field
+              type="password"
+              name="password"
+              id="userPassword"
+              placeholder="Password"
+              component={this.renderInput}
+            />
+          </FormGroup>
+          <Button>Log In</Button>
+        </Form>
+      </Container>
     );
   }
 }
 
-const validate = (formValues) => {
+const validate = formValues => {
   const errors = {};
   if (!formValues.email) {
     errors.title = 'You must enter an email';
@@ -61,7 +75,7 @@ const validate = (formValues) => {
 };
 
 const formWrapped = reduxForm({
-  form: 'userLogin',
+  form: 'UserLogin',
   validate,
 })(UserLogIn);
 
