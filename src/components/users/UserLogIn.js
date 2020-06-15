5098307a7a056799a517b13d6037a74ec346d092
KennethLloyd <verastiguelloyd@gmail.com>
2020-06-15 16:54:29 +0800

    Add project logo and label to login page initial styling

--- src/components/users/UserLogIn.js
+++ src/components/users/UserLogIn.js
@@ -1,82 +1,107 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form, FormGroup, Input, Container } from 'reactstrap';
+import {
+  Button,
+  Form,
+  FormText,
+  FormFeedback,
+  FormGroup,
+  Input,
+  Container,
+  Col,
+  Row
+} from 'reactstrap';
 
 import { logIn } from '../../actions';
+import '../../index.css';
+import projectLogo from '../../images/project-logo.svg';
+import projectLabel from '../../images/project-label.svg';
 
 class UserLogIn extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div>
-          <div>{error}</div>
-        </div>
-      );
-    }
-  }
-
   renderInput = formProps => {
-    console.log(formProps);
-    const { input, meta, type, id, placeholder } = formProps;
+    const { input, type, id, placeholder } = formProps;
 
     return (
       <div>
-        <Input {...input} type={type} id={id} placeholder={placeholder} />
-        <div>{this.renderError(meta)}</div>
+        <Input
+          {...input}
+          type={type}
+          id={id}
+          placeholder={placeholder}
+          required
+        />
       </div>
     );
   };
 
   onSubmit = formValues => {
-    console.log(formValues);
     this.props.logIn(formValues);
   };
 
   render() {
     return (
       <Container>
-        <Form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-          <FormGroup>
-            <Field
-              type="email"
-              name="email"
-              id="userEmail"
-              placeholder="Email Address"
-              component={this.renderInput}
+        <Row className="align-items-center" style={{ height: '100vh' }}>
+          <Col md={{ size: 4, offset: 4 }}>
+            <img
+              className="img-fluid mx-auto d-block"
+              src={projectLogo}
+              style={{ width: '70%' }}
             />
-          </FormGroup>
-          <FormGroup>
-            <Field
-              type="password"
-              name="password"
-              id="userPassword"
-              placeholder="Password"
-              component={this.renderInput}
+            <img
+              className="img-fluid mx-auto d-block"
+              src={projectLabel}
+              style={{ width: '60%' }}
             />
-          </FormGroup>
-          <Button>Log In</Button>
-        </Form>
+            <Form
+              onSubmit={this.props.handleSubmit(this.onSubmit)}
+              className="mt-5"
+            >
+              <FormGroup>
+                <Field
+                  type="email"
+                  name="email"
+                  id="userEmail"
+                  placeholder="Email Address"
+                  component={this.renderInput}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Field
+                  type="password"
+                  name="password"
+                  id="userPassword"
+                  placeholder="Password"
+                  component={this.renderInput}
+                />
+              </FormGroup>
+              <FormText color="secondary" className="text-right">
+                <Link to="/forgot-password" className="text-secondary">
+                  Forgot password?
+                </Link>
+              </FormText>
+              <div className="text-center mt-2">
+                <Button color="primary">Log In</Button>
+              </div>
+              <FormText color="secondary" className="text-center">
+                Don't have an account?{' '}
+                <Link to="/sign-up" className="text-secondary">
+                  Sign Up
+                </Link>
+              </FormText>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     );
   }
 }
 
-const validate = formValues => {
-  const errors = {};
-  if (!formValues.email) {
-    errors.title = 'You must enter an email';
-  }
-  if (!formValues.password) {
-    errors.password = 'You must enter a password';
-  }
-  return errors;
-};
-
 const formWrapped = reduxForm({
-  form: 'UserLogin',
-  validate,
+  form: 'UserLogin'
 })(UserLogIn);
 
 export default connect(null, { logIn })(formWrapped);
