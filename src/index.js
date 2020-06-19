f226dc5f88c26e7256a132012d70d0034eedf867
KennethLloyd <verastiguelloyd@gmail.com>
2020-06-20 07:28:40 +0800

    Add redux-persist

--- src/index.js
+++ src/index.js
@@ -2,20 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  reducers,
-  composeEnhancers(applyMiddleware(reduxThunk))
+  persistedReducer,
+  composeEnhancers(applyMiddleware(reduxThunk)),
 );
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
-  document.querySelector('#root')
+  document.querySelector('#root'),
 );
