0238b8840681073a3cb353d778cefc37616ee0e9
KennethLloyd <verastiguelloyd@gmail.com>
2020-06-20 08:17:16 +0800

    Add mood filter and search

--- src/components/home/SearchFilter.js
+++ src/components/home/SearchFilter.js
@@ -6,13 +6,17 @@ import {
   DropdownMenu,
   DropdownItem,
   Container,
+  Form,
+  Button,
+  Label,
+  Input,
 } from 'reactstrap';
 
 class SearchFilter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mood: 'All',
+      mood: 'ALL',
       dropdownOpen: false,
     };
   }
@@ -23,26 +27,60 @@ class SearchFilter extends React.Component {
 
   renderDropDown() {
     return (
-      <div className="d-flex">
-        <p>Mood:</p>
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle caret>{this.state.mood}</DropdownToggle>
-          <DropdownMenu>
-            {this.props.moods
-              ? this.props.moods.map((mood) => {
-                  return (
-                    <DropdownItem key={mood._id}>{mood.name}</DropdownItem>
-                  );
-                })
-              : ''}
-          </DropdownMenu>
-        </Dropdown>
+      <div className="d-flex align-items-center">
+        <Label className="mt-auto mb-auto mr-2">Mood: </Label>
+        <Input bsSize="sm" type="select" name="select">
+          <option onClick={() => this.setState({ mood: 'ALL' })}>ALL</option>
+          {this.props.moods
+            ? this.props.moods.map((mood) => {
+                return (
+                  <option
+                    key={mood._id}
+                    onClick={() => this.setState({ mood: mood.name })}
+                  >
+                    {mood.name}
+                  </option>
+                );
+              })
+            : ''}
+        </Input>
       </div>
     );
   }
 
+  renderSearch() {
+    return (
+      <Form
+        className="inline my-2 my-lg-0 d-flex align-items-center"
+        onClick={(e) => e.preventDefault()}
+      >
+        <Input
+          size="sm"
+          className="mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <Button
+          size="sm"
+          outline
+          color="secondary"
+          className="my-2 my-sm-0 ml-2 ml-md-0"
+          type="submit"
+        >
+          Search
+        </Button>
+      </Form>
+    );
+  }
+
   render() {
-    return <Container>{this.renderDropDown()}</Container>;
+    return (
+      <Container className="d-xs-flex d-md-flex justify-content-md-between mt-3">
+        {this.renderDropDown()}
+        {this.renderSearch()}
+      </Container>
+    );
   }
 }
 
