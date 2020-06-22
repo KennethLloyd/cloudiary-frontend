c7ac28ce0c38346111f79aa498c773f47f50a766
KennethLloyd <verastiguelloyd@gmail.com>
2020-06-22 11:35:24 +0800

    Change Homepage component from class to function to use hooks

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,35 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'reactstrap';
 import { connect } from 'react-redux';
 import Header from './Header';
 import Entries from './Entries';
 import { fetchMoods } from '../../actions';
 
-class HomePage extends React.Component {
-  state = {
-    activeView: 'Entries'
-  };
+const HomePage = props => {
+  const [activeView, changeActiveView] = useState('Entries');
 
-  componentDidMount() {
-    this.props.fetchMoods(this.props.token);
-  }
+  useEffect(() => {
+    props.fetchMoods(props.token);
+  }, []);
 
-  changeActiveView = activeView => {
-    this.setState({ activeView });
-    console.log(`Active view is now ${activeView}`);
-  };
-
-  render() {
-    return (
-      <div>
-        <Header setActive={this.changeActiveView} />
-        <Container fluid>
-          <div>{this.state.activeView === 'Entries' ? <Entries /> : ''}</div>
-        </Container>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Header setActive={changeActiveView} />
+      <Container fluid>
+        <div>{activeView === 'Entries' ? <Entries /> : ''}</div>
+      </Container>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return { token: state.currentUser.token };
