ff0b403ad6c551a15ed12c2da6eaf91213f34711
KennethLloyd <verastiguelloyd@gmail.com>
2020-07-01 15:15:03 +0800

    Add logo icons

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,4 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import userReducer from './userReducer';
 import moodReducer from './moodReducer';
 import activityReducer from './activityReducer';
@@ -7,7 +6,6 @@ import entryReducer from './entryReducer';
 import errorReducer from './errorReducer';
 
 export default combineReducers({
-  form: formReducer,
   currentUser: userReducer,
   moods: moodReducer,
   activities: activityReducer,
