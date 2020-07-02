cfa05214b2b7cf070fd7ed29764c0c001f7cffee
KennethLloyd <verastiguelloyd@gmail.com>
2020-07-02 19:05:52 +0800

    Add greetings alert upon login

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Alert } from 'reactstrap';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import Header from './Header';
 import Entries from './Entries';
 import { fetchMoods } from '../../actions/moodActions';
@@ -8,10 +9,35 @@ import { fetchActivities } from '../../actions/activityActions';
 
 const HomePage = (props) => {
   const [activeView, changeActiveView] = useState('Entries');
+  const [alertOpen, setAlertOpen] = useState(true);
+
+  const greet = () => {
+    const time = moment().format('HH:mm');
+    const hour = parseInt(time.split(':')[0]);
+
+    if (hour >= 0 && hour < 12) {
+      return 'Good morning';
+    } else if (hour >= 12 && hour < 18) {
+      return 'Good afternoon';
+    } else {
+      return 'Good evening';
+    }
+  };
+
+  const closeAlert = () => {
+    setAlertOpen(false);
+  };
 
   useEffect(() => {
     props.fetchMoods();
     props.fetchActivities();
+
+    if (alertOpen) {
+      const timer = setTimeout(() => {
+        setAlertOpen(false);
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
   }, []);
 
   return (
@@ -19,9 +45,18 @@ const HomePage = (props) => {
       <Header setActive={changeActiveView} />
       <Container fluid>
         <div>{activeView === 'Entries' ? <Entries /> : ''}</div>
+        <Alert className="welcome-alert" isOpen={alertOpen} toggle={closeAlert}>
+          {`${greet()} ${props.firstName}!`}
+        </Alert>
       </Container>
     </div>
   );
 };
 
-export default connect(null, { fetchMoods, fetchActivities })(HomePage);
+const mapStateToProps = (state) => {
+  return { firstName: state.currentUser.info.firstName };
+};
+
+export default connect(mapStateToProps, { fetchMoods, fetchActivities })(
+  HomePage,
+);
